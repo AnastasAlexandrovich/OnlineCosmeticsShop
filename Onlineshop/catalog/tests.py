402853a2509bc6599afab2a913d4b09f77ae3497
AnastasAlexandrovich <a-khoroshilova@mail.ru>
2022-01-19 02:10:19 +0400

    тестирование с моками

--- Onlineshop/catalog/tests.py
+++ Onlineshop/catalog/tests.py
@@ -1,8 +1,4 @@
-import tempfile
-
-import pytest
 from django.test import TestCase
-from pytest_postgresql import factories
 
 from .models import Product, Manufacturer, Type
 
@@ -11,9 +7,15 @@ class ProductListViewTest(TestCase):
 
     def setUp(self):
         a = Manufacturer.objects.create(name="Loreal")
-        t = Type.objects.create(name="vvvv")
-        Product.objects.create(name="FRFR", description="d", price=10, seller=a, in_stock=1, type=t)
+        t = Type.objects.create(name="Помада")
+        Product.objects.create(name="супер помада 1", description="1", price=10, seller=a, in_stock=1, type=t)
+        Product.objects.create(name="супер помада 2", description="2", price=10, seller=a, in_stock=3, type=t)
+        Product.objects.create(name="супер помада 3", description="3", price=10, seller=a, in_stock=5, type=t)
 
     def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
+        resp = self.client.get('/products/')
+        self.assertEqual(resp.status_code, 200)
+        product_list = resp.json()
+        self.assertEqual(len(product_list), 3)
+        pass
 
--- Onlineshop/order/tests.py
+++ Onlineshop/order/tests.py
@@ -1,3 +1,22 @@
+import mock
 from django.test import TestCase
+from django_mock_queries.query import MockSet, MockModel
 
-# Create your tests here.
+from .models import Order
+
+
+class OrderListViewTest(TestCase):
+    qs = MockSet(
+        MockModel(id=1, orderer=1, date_of_order="01.02.2022", address="Some street",
+                  date_of_delivery="02.02.2022", product_amount="1", status='Новый'))
+
+    @mock.patch('order.models.Order.objects.all', qs)
+    def test_some_function(self):
+        orders = Order.objects.all
+        self.assertEqual(len(orders), 1)
+
+
+
+
+
+
